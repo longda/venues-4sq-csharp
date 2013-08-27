6e8adad90911b9d6740889b4d3e0a416fb21c210
David Long <longda@gmail.com>
2013-08-27 14:59:44 -0700

    added in table sorter fakery

--- le-secret-venue/App_Start/BundleConfig.cs
+++ le-secret-venue/App_Start/BundleConfig.cs
@@ -18,6 +18,9 @@ namespace le_secret_venue
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery.tablesorter.js").Include(
+                "~/Scripts/jquery.tablesort.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
--- le-secret-venue/Content/js/app.js
+++ le-secret-venue/Content/js/app.js
@@ -2,29 +2,16 @@
 
 $(document).ready(function () {
 
+    $('#data-table').tablesorter();
+    $('#myTable').tablesorter();
+
     $('#btn-search').click(function () {
-        console.log('search button clicked...');
-        //var data, data_html;
-
-        //data = {
-        //  "name": "Tripel",
-        //  "address": "Playa Vista, CA",
-        //  "rating": "9.1",
-        //  "num_of_reviews": "175",
-        //  "num_of_checkins": "97"
-        //};
-
-        //data_html = ich.data_temp(data);
-
-        //$('#data-table-body').append(data_html);
-
 
         // new logic
         var near = $('#near').val();
         $.ajax({
             url: "http://localhost:52382/data/explore/" + near
         }).done(function (data) {
-            //console.log('made ajax call: ', data);
             $('#data-table-body').html('');
 
 
@@ -32,7 +19,14 @@ $(document).ready(function () {
                 data_html = ich.data_temp(element);
                 $('#data-table-body').append(data_html);
             });
+
+            $('#data-table').trigger('update');
         });
+
+        
+        //$('#data-table').tablesorter();
+
+        return false;
     });
 
 });
